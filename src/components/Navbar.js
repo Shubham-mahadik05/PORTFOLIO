aa6b55810f6936c33027b4d4184b6b9408d8e825
Mr.shubham <113901114+Shubham-mahadik05@users.noreply.github.com>
2025-09-28 21:00:32 +0530

    not updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href="https://soumyajitblogs.vercel.app/"
+                href="https://portfolio-cey714bpo-shubhams-projects-c333bdcf.vercel.app/"
                 target="_blank"
                 rel="noreferrer"
               >
